65e0033066ee870b7aba332f1083478acf219e52
Manuel <Manuel@192.168.1.75>
2019-12-30 16:25:02 -0600

    Movimiento de enemigos finalizado

--- src/pruebasbomberman/Enemy.java
+++ src/pruebasbomberman/Enemy.java
@@ -1,6 +1,7 @@
 
 package pruebasbomberman;
 
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JTable;
@@ -12,6 +13,7 @@ public class Enemy extends Thread{
     private String txt = "E";
     int posX, posY;
     boolean alive = true;
+    Random dir = new Random();
     
     public Enemy(JTable map, int posX, int posY){
         this.map = map;
@@ -23,92 +25,54 @@ public class Enemy extends Thread{
     @Override
     public void run(){
         while(checkAlive()){
-            //
-            if(posX == 0){
-                if(map.getValueAt(posY - 1, posX).equals("N")){
-                    map.setValueAt("E", posY - 1, posX);
-                    map.setValueAt("N", posY, posX);
-                    posY--;
-                }else if(map.getValueAt(posY + 1, posX).equals("N")){
-                    map.setValueAt("E", posY + 1, posX);
-                    map.setValueAt("N", posY, posX);
-                    posY++;
-                }else if(map.getValueAt(posY, posX + 1).equals("N")){
-                    map.setValueAt("E", posY, posX + 1);
-                    map.setValueAt("N", posY, posX);
-                    posX++;
+            int dir = this.dir.nextInt(4);
+            System.out.println(dir);
+            /* 
+                0 UP
+                1 DOWN
+                2 RIGHT
+                3 LEFT
+            */
+            
+            if(dir == 0){
+                if(posY != 0){
+                    if(map.getValueAt(posY - 1, posX).equals("N")){
+                        System.out.println("Arriba");
+                        map.setValueAt("E", posY - 1, posX);
+                        map.setValueAt("N", posY, posX);
+                        posY--;
+                    }                    
                 }
-            }
-            //
-            else if(posX == 11){
-                if(map.getValueAt(posY - 1, posX).equals("N")){
-                    map.setValueAt("E", posY - 1, posX);
-                    map.setValueAt("N", posY, posX);
-                    posY--;
-                }else if(map.getValueAt(posY + 1, posX).equals("N")){
-                    map.setValueAt("E", posY + 1, posX);
-                    map.setValueAt("N", posY, posX);
-                    posY++;
-                }else if(map.getValueAt(posY, posX - 1).equals("N")){
-                    map.setValueAt("E", posY, posX + 1);
-                    map.setValueAt("N", posY, posX);
-                    posX--;
+            }else if(dir == 1){
+                if(posY != 11){
+                    if(map.getValueAt(posY + 1, posX).equals("N")){
+                        System.out.println("Abajo");
+                        map.setValueAt("E", posY + 1, posX);
+                        map.setValueAt("N", posY, posX);
+                        posY++;
+                    }                    
                 }
-            }
-            //
-            else if(posY == 0){
-                System.out.println(posX);
-                System.out.println(posY);
-                if(map.getValueAt(posY + 1, posX).equals("N")){
-                    map.setValueAt("E", posY + 1, posX);
-                    map.setValueAt("N", posY, posX);
-                    posY++;
-                }else if(map.getValueAt(posY, posX + 1).equals("N")){
-                    map.setValueAt("E", posY, posX + 1);
-                    map.setValueAt("N", posY, posX);
-                    posX++;
-                }else if(map.getValueAt(posY, posX - 1).equals("N")){
-                    map.setValueAt("E", posY, posX - 1);
-                    map.setValueAt("N", posY, posX);
-                    posX--;
+            }else if(dir == 2){
+                if(posX != 11){
+                    if(map.getValueAt(posY, posX + 1).equals("N")){
+                        System.out.println("Derecha");
+                        map.setValueAt("E", posY, posX + 1);
+                        map.setValueAt("N", posY, posX);
+                        posX++;
+                    }                    
                 }
-            }
-            //
-            else if(posY == 11){
-                if(map.getValueAt(posY - 1, posX).equals("N")){
-                    map.setValueAt("E", posY - 1, posX);
-                    map.setValueAt("N", posY, posX);
-                    posY--;
-                }else if(map.getValueAt(posY, posX + 1).equals("N")){
-                    map.setValueAt("E", posY, posX + 1);
-                    map.setValueAt("N", posY, posX);
-                    posX++;
-                }else if(map.getValueAt(posY, posX - 1).equals("N")){
-                    map.setValueAt("E", posY, posX - 1);
-                    map.setValueAt("N", posY, posX);
-                    posX--;
-                }
-            }else{
-                if(map.getValueAt(posY - 1, posX).equals("N")){
-                    map.setValueAt("E", posY - 1, posX);
-                    map.setValueAt("N", posY, posX);
-                    posY--;
-                }else if(map.getValueAt(posY + 1, posX).equals("N")){
-                    map.setValueAt("E", posY + 1, posX);
-                    map.setValueAt("N", posY, posX);
-                    posY++;
-                }else if(map.getValueAt(posY, posX + 1).equals("N")){
-                    map.setValueAt("E", posY, posX + 1);
-                    map.setValueAt("N", posY, posX);
-                    posX++;
-                }else if(map.getValueAt(posY, posX - 1).equals("N")){
-                    map.setValueAt("E", posY, posX - 1);
-                    map.setValueAt("N", posY, posX);
-                    posX--;
+            }else if(dir == 3){
+                if(posX != 0){
+                    if(map.getValueAt(posY, posX - 1).equals("N")){
+                        System.out.println("Izquierda");
+                        map.setValueAt("E", posY, posX - 1);
+                        map.setValueAt("N", posY, posX);
+                        posX--;
+                    }                    
                 }
             }
             try {
-                sleep(3000);
+                sleep(1000);
             } catch (InterruptedException ex) {
                 Logger.getLogger(Enemy.class.getName()).log(Level.SEVERE, null, ex);
             }
