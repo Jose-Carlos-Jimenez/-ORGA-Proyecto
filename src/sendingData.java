ff1ebdc8de7c88e4ca60443fda1b86d47c2a0ecd
Manuel <Manuel@192.168.1.75>
2020-01-01 23:26:09 -0600

    Envio de datos realizado. Falta ver como es lo de utilizar el default package. Juego aun sin finalizar por victoria

---
+++ src/sendingData.java
@@ -0,0 +1,49 @@
+
+
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JTable;
+import static pruebasbomberman.PruebasBomberman.playing;
+
+public class sendingData extends Thread{
+    
+    private JTable map;
+    private pPort lpt;
+    
+    public sendingData(JTable map){
+        this.map = map;
+        this.lpt = new pPort();
+        lpt.setAllDataBits((short)0);
+    }
+    
+    @Override
+    public void run(){
+        int x = 0;
+        //while(playing){
+            
+            for(int i = 0; i < 12; i++){
+                if(map.getValueAt(11 - i, x).equals("")){
+                    this.lpt.setPin((short)2, (short)0);                    
+                }else{
+                    this.lpt.setPin((short)2, (short)1);
+                }
+                if(i == 11 && x != 11){
+                    i = -1;
+                    x++;
+                }else if(i == 11 && x == 11){
+                    i = -1;
+                    x = 0;
+                }
+            }  
+        //}
+        if(!playing){
+            try {
+                this.finalize();
+            } catch (Throwable ex) {
+                Logger.getLogger(sendingData.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+}
